860c01ed8fda7a5a6c75912ffcc2e64dbaae2bf9
David Lambert <david.lambert@lajungle.fr>
2023-03-28 15:01:59 +0200

    Test

--- index.php
+++ index.php
@@ -5,4 +5,14 @@ $e = getenv();
 var_dump($e);
 echo '======';
 var_dump($_SERVER);
-die();
+
+$host = 'azrimdbrcnews.mariadb.database.azure.com';
+$user = getenv('APPSETTING_MariaDbUser');
+$password = getenv('APPSETTING_MariaDbPwd');
+$db = null;
+
+var_dump($user, $password);
+
+$mysqli = new mysqli($host, $user, $password, $db);
+
+var_dump($mysqli);
