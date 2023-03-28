f7ff02d7561436a08d8260ba456f1f31905c5c7b
David Lambert <david.lambert@lajungle.fr>
2023-03-28 15:19:12 +0200

    Test

--- index.php
+++ index.php
@@ -11,8 +11,16 @@ $user = getenv('APPSETTING_MariaDbUser');
 $password = getenv('APPSETTING_MariaDbPwd');
 $db = null;
 
+// $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "azrimdbrcnews.mariadb.database.azure.com", "admins@azrimdbrcnews", {your_password}, {your_database}, 3306);
+
 var_dump($user, $password);
 
 $mysqli = new mysqli($host, $user, $password, $db);
-
 var_dump($mysqli);
+
+$user = 'admins@azrimdbrcnews';
+$mysqli2 = new mysqli($host, $user, 'Password@12345', $db);
+var_dump($mysqli2);
+
+$mysqli3 = new mysqli($host, $user, 'Password@12345', 'test');
+var_dump($mysqli3);
