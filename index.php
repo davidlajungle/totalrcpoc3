3dfde7d0f9f28b447351d2f13348416d4c247d79
David Lambert <david.lambert@lajungle.fr>
2023-03-28 15:43:39 +0200

    Test

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $db = null;
 // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "azrimdbrcnews.mariadb.database.azure.com", "admins@azrimdbrcnews", {your_password}, {your_database}, 3306);
 
 var_dump($user, $password);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // $mysqli = mysqli_init();
 // mysqli_ssl_set($mysqli, null, null, null, null, null);
@@ -22,7 +23,7 @@ var_dump($user, $password);
 
 $user = 'admins@azrimdbrcnews';
 $password = 'Password@12345';
-$mysqli2 = mysqli_init();
-mysqli_ssl_set($mysqli2, null, null, null, null, null);
-$mysqli2 = mysqli_real_connect($mysqli2, $host, $user, $password, $db, 3306);
+// $mysqli2 = mysqli_init();
+// mysqli_ssl_set($mysqli2, null, null, null, null, null);
+$mysqli2 = new mysqli($host, $user, $password, $db, 3306);
 var_dump($mysqli2);
