5d398dd80cd7bc2c016bac8b3e3fd72d3b44006f
David Lambert <david.lambert@lajungle.fr>
2023-04-04 11:04:35 +0200

    Up

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 echo '<pre>';
-// $e = getenv();
-// var_dump($e);
+$e = getenv();
+var_dump($e);
 // echo '======';
 // var_dump($_SERVER);
 
@@ -13,13 +13,12 @@ $db = null;
 
 // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "azrimdbrcnews.mariadb.database.azure.com", "admins@azrimdbrcnews", {your_password}, {your_database}, 3306);
 
-$user = 'admins@azrimdbrcnews';
-$password = 'Password@12345';
+// $user = 'admins@azrimdbrcnews';
+// $password = 'Password@12345';
 var_dump($user, $password);
 
 $mysqli = mysqli_init();
 // $mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 $mysqli->ssl_set(NULL, NULL, "", NULL, NULL);
-var_dump($mysqli);
 $mysqli->real_connect($host, $user, $password, null, 3306, null, MYSQLI_CLIENT_SSL | MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 var_dump($mysqli);
